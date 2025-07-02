c4ca290ee9bed29ae4e66de59107434c7a665cb4
maicon501 <maicondmonteiro@gmail.com>
2025-07-01 21:24:57 -0300

    mudança no codigo para alteraçao do valor

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
 
-function convertVelues() {
+function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
   const CurrencyValueToConvert = document.querySelector(".montante-real"); // Valor Real
   const CurrencyValueConverted = document.querySelector(".montante"); // Outras Moedas
+  console.log(inputCurrencyValue, currencySelect.value);
+  const dolarToday = 5.0; // Valor do Dólar hoje
+  // Valor do Euro hoje
+  const euroToday = 6.0;
 
-  const dolarToday = 5.5;
+  let convertedValue;
 
-  const convertedValue = inputCurrencyValue / dolarToday;
+  if (currencySelect.value === "US$ Dólar americano") {
+    convertedValue = inputCurrencyValue / dolarToday;
+  } else if (currencySelect.value === "€ Euro") {
+    convertedValue = inputCurrencyValue / euroToday;
+  }
 
   CurrencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
@@ -20,4 +29,4 @@ function convertVelues() {
   }).format(convertedValue);
 }
 
-convertButton.addEventListener("click", convertVelues);
+convertButton.addEventListener("click", convertValues);
